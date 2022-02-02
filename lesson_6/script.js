dbbc90f58ab6395d787b996aeb5152301c7a3a63
Jekasss <eajekassol@ya.ru>
2022-02-02 21:06:05 +0300

    lesson_6

---
+++ lesson_6/script.js
@@ -0,0 +1,49 @@
+let age = prompt("Введите Ваш возраст")
+
+if (age > 18) {
+    alert("Вы допущены")
+}
+
+if (age < 18) {
+    alert("Вы не допущены")
+}
+
+if (true) {
+    alert ("true")
+}
+
+if (false) {
+    alert ("false")
+}
+
+if (age >= 18) {
+    alert("Вы допущены")    
+} else {
+    alert ("Вы не допущены")
+}
+
+if (1) {
+    alert("One")    
+} else {
+    alert ("Zero")
+}
+
+if (0) {
+    alert("One")    
+} else {
+    alert ("Zero")
+}
+
+if (age == 18) {
+    alert("С совершеннолетием!")    
+} else if (age > 18) {
+    alert ("Вы допущены!")
+} else {
+    alert ("Вы недопущены!")
+}
+
+if (" ") {
+    alert("One")    
+} else {
+    alert ("Zero")
+}
