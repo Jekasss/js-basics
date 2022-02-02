f4695d0d26aea6df2b67f1707f8b9a7ff59874e9
Jekasss <eajekassol@ya.ru>
2022-02-02 20:40:03 +0300

    lesson_5

---
+++ lesson_5/script.js
@@ -0,0 +1,34 @@
+//операторы сравнения всгда бинарные
+
+console.log(5 > 2);
+console.log(5 < 2);
+
+let a = 5 > 2;
+console.log(a);
+
+console.log(0>=0);
+console.log(0<=0);
+
+console.log(0 != 0);
+
+console.log(0 == 0);
+
+console.log(0 == "0");
+console.log(0 != "1");
+
+console.log(0 === "0");
+console.log(0 !== "1");
+console.log(0 === 0);
+console.log(0 !== 1);
+
+console.log("q" > "a");
+console.log("aq" > "aa");
+console.log("aa" > "aaaa")
+
+console.log(null== undefined);
+console.log(null=== undefined);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
